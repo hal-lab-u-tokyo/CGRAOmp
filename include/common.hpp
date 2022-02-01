33c1f84bb52f5164ca32e18d3e01c1b3d2c968e2
tkojima <tkojima@ikura.hal.ipc.i.u-tokyo.ac.jp>
2022-02-01 19:59:58 +0900

    delete test code to work for github actions

---
+++ include/common.hpp
@@ -0,0 +1,40 @@
+/*
+*    MIT License
+*    
+*    Copyright (c) 2022 Amano laboratory, Keio University & Processor Research Team, RIKEN Center for Computational Science
+*    
+*    Permission is hereby granted, free of charge, to any person obtaining a copy of
+*    this software and associated documentation files (the "Software"), to deal in
+*    the Software without restriction, including without limitation the rights to
+*    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+*    of the Software, and to permit persons to whom the Software is furnished to do
+*    so, subject to the following conditions:
+*    
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*    
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
+*    
+*    File:          /include/common.hpp
+*    Project:       CGRAOmp
+*    Author:        Takuya Kojima in Amano Laboratory, Keio University (tkojima@am.ics.keio.ac.jp)
+*    Created Date:  31-01-2022 13:45:57
+*    Last Modified: 31-01-2022 16:35:44
+*/
+
+#ifndef CGRA_OMP_COMMON_H
+#define CGRA_OMP_COMMON_H
+
+#define ERR_MSG_PREFIX "CGRAOmpPass \x1B[31m\033[1mError\033[0m: "
+#define INFO_DEBUG_PREFIX "\t[INFO]: "
+#define WARN_DEBUG_PREFIX "\t[WARN]: "
+#define ERR_DEBUG_PREFIX "\t[ERROR]: "
+#define DBG_DEBUG_PREFIX "\t[DEBUG]: "
+
+#endif //CGRA_OMP_COMMON_H
