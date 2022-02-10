383dbac6edc2ae0a790329248c0d0d89246f6bdd
tkojima <tkojima@sawara.hal.ipc.i.u-tokyo.ac.jp>
2022-02-11 00:43:44 +0900

    add a sample of DFGPass

---
+++ src/Passes/HelloDFGPass/HelloDFGPass.hpp
@@ -0,0 +1,44 @@
+/*
+*    MIT License
+*    
+*    Copyright (c) 2022 Amano laboratory, Keio University & Processor Research Team, RIKEN Center for Computational Science
+*    
+*    Permission is hereby granted, free of charge, to any person obtaining a copy of
+*    this software and associated documentation files (the "Software"), to deal in
+*    the Software without restriction, including without limitation the rights to
+*    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+*    of the Software, and to permit persons to whom the Software is furnished to do
+*    so, subject to the following conditions:
+*    
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*    
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
+*    
+*    File:          /src/Passes/TestDFGPass/test.hpp
+*    Project:       CGRAOmp
+*    Author:        Takuya Kojima in Amano Laboratory, Keio University (tkojima@am.ics.keio.ac.jp)
+*    Created Date:  30-01-2022 20:55:50
+*    Last Modified: 31-01-2022 13:39:57
+*/
+#include "DFGPass.hpp"
+#include "CGRADataFlowGraph.hpp"
+
+#include "llvm/IR/PassManager.h"
+
+using namespace llvm;
+using namespace CGRAOmp;
+
+
+class HelloDFGPass : public PassInfoMixin<HelloDFGPass> {
+	public:
+		bool run(CGRADFG &G, Loop &L, FunctionAnalysisManager &FAM,
+									LoopAnalysisManager &LAM,
+									LoopStandardAnalysisResults &AR);
+};
